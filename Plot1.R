99dee0068c612cb2b95749ad46c5c16a9b768e36
Jeyanthpranav <jeyanthpranavrsuresh@gmail.com>
2016-10-21 00:32:00 +0530

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -12,7 +12,9 @@ plotData <- fread(filePath,sep=";",nrows = -1, na.strings = "?",stringsAsFactors
 finalData <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
 SetTime <-strptime(paste(finalData$Date, finalData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
 finalData <- cbind(SetTime, finalData)
-
+		
+png("plot1.png", width=480, height=480)
 hist(finalData$Global_active_power, main = "Global Active Power", ylab = "Frequency", 
     xlab = "Global Active Power (kilowatts)", col = "red", breaks = 13, ylim = c(0, 
         1200), xlim = c(0, 6), xaxp = c(0, 6, 3))
+dev.off()
