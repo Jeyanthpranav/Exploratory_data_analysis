cf2fc74aa86719357b406b22580788debc083392
Jeyanthpranav <jeyanthpranavrsuresh@gmail.com>
2016-10-21 00:32:41 +0530

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -13,4 +13,6 @@ finalData <- plotData[plotData$Date %in% c("1/2/2007","2/2/2007"),]
 SetTime <-strptime(paste(finalData$Date, finalData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
 finalData <- cbind(SetTime, finalData)
 
+png("plot2.png", width=480, height=480)
 plot(finalData$SetTime, finalData$Global_active_power, type = "l", xlab = "", ylab = "Global Active Power (kilowatts)")
+dev.off()
